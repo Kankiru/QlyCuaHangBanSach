24a1bfa9039630b6cf40e31ced2145141a36ca13
danghonghuy <huyvtm580@gmail.com>
2024-04-09 17:40:24 +0700

    lại

---
+++ SqlNhom8.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{6F76A786-1DDD-4FCC-BAFA-6CE138D63DB8}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {FD4C1A59-DED7-4247-B061-7E1961119DFD}
+	EndGlobalSection
+EndGlobal
